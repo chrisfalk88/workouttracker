80cc4c87fc1fd4e3f1488f854938346f92d2cf43
Chris Falk <christophersfalk@gmail.com>
2020-12-08 11:05:25 -0700

    added html routes file and configured them

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 // Exporting an object containing all of our models
+// when we reference DB it would essentially be lines 4-6
 
 module.exports = {
   Workout: require("./Workout")
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+require("./routes/html.js")(app);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.use(require("./routes/api.js"));
